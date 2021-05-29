dbd95ab6842c2bcea65029ed70fc46be8e1ff132
Oliver Quiel <oliverquiel@gmail.com>
2021-05-29 09:23:42 -0500

    Subida de archivo con ipfs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {DIDManager, IPLDManager} from 'xdv-universal-wallet';
 
-function App() {
+async function App() {
+  const [selectedFile, setSelectedFile] = useState();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const didManager = new DIDManager();
+  const pin = this.pin;
+  const didRSA = await didManager.create3ID_RSA();
+  await didRSA.did.authenticate();
+
+  const ipfsManager = new IPLDManager(didRSA.did);
+  await ipfsManager.start('http://ifesa.ipfs.pa:5001');
+  this.ipfs = ipfsManager;
+
+  const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsFilePicked(true);
+	};
+
+	const handleSubmission = async () => {
+    const cid = await ipfsManager.addSignedObject(u8array, {
+      // file name
+        name: file.name,
+        // mime 
+        contentType: file.type,
+        // file last modified
+        lastModified: new Date(file.lastModified),
+        // optional, PEM certificate
+        certificate: didRSA.certificate,
+      } );
+	};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +50,26 @@ function App() {
           Learn React
         </a>
       </header>
+        <div>
+        <input type="file" name="file" onChange={changeHandler} />
+        {isFilePicked ? (
+				<div>
+					<p>Filename: {selectedFile.name}</p>
+					<p>Filetype: {selectedFile.type}</p>
+					<p>Size in bytes: {selectedFile.size}</p>
+					<p>
+						lastModifiedDate:{' '}
+						{selectedFile.lastModifiedDate.toLocaleDateString()}
+					</p>
+				</div>
+			) : (
+				<p>Select a file to show details</p>
+			)}
+        <div>
+          <button onClick={handleSubmission}>Submit</button>
+        </div>
+      </div>
+
     </div>
   );
 }
